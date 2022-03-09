189671e29591bf1dbdb7f13550f730bd308ae95e
Tom <thomas.spencer@codurance.com>
2022-03-09 19:37:50 +0000

    add solution

--- src/main/java/com/codurance/Stack.java
+++ src/main/java/com/codurance/Stack.java
@@ -1,15 +1,38 @@
 package com.codurance;
+import static java.lang.System.arraycopy;
 
 public class Stack {
 
-    private boolean isEmpty = true;
     private int size = 0;
+    private int[] elements = new int[0];
 
     public boolean isEmpty() {
-        return isEmpty;
+        return size == 0;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public void push(int element) {
+        if (size >= elements.length) {
+            increaseSize();
+        }
+
+        elements[size++] = element;
+    }
+
+    private void increaseSize() {
+        int[] newElements = new int[size + 1];
+        arraycopy(elements, 0, newElements, 0, size);
+        elements = newElements;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new Underflow();
+        }
+
+        return elements[--size];
     }
 }
--- src/test/java/com/codurance/StackTest.java
+++ src/test/java/com/codurance/StackTest.java
@@ -1,17 +1,67 @@
 package com.codurance;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class StackTest {
+    private Stack stack;
+
+    @BeforeEach
+    void setUp() {
+        stack = new Stack();
+    }
+
     @Test
     void newlyCreatedStackShouldBeEmpty() {
-        Stack stack = new Stack();
         assertThat(stack.isEmpty())
                 .isTrue();
-        assertThat(stack.getSize())
-                .isEqualTo(0);
+    }
+
+    @Test
+    void afterOnePush_StackIsNotEmpty() {
+        stack.push(0);
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    void poppingEmptyStack_throwsUnderflow() {
+        assertThatThrownBy(stack::pop)
+                .isInstanceOf(Underflow.class);
+    }
+
+    @Test
+    void afterOnePushAndOnePop_isEmpty() {
+        stack.push(0);
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    void afterTwoPushes_sizeIsTwo() {
+        stack.push(0);
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
+
+
+    @Test
+    void afterPushingX_stackPopsX() {
+        stack.push(23);
+        assertEquals(23, stack.pop());
+        stack.push(42);
+        assertEquals(42, stack.pop());
+    }
+
+    @Test
+    void afterPushingXAndY_stackPopsYAndX() {
+        stack.push(23);
+        stack.push(42);
+        assertEquals(42, stack.pop());
+        assertEquals(23, stack.pop());
     }
 }
